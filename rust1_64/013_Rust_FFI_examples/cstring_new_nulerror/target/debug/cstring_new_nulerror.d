5ce889580ba64c8f8b4aba4116e2a8db0be8c3b3
YoungHa Kim <ytok1108@aol.com>
2022-10-02 22:35:32 +0900

    nul byte found in provided data at position: 1__

--- rust1_64/013_Rust_FFI_examples/cstring_new_nulerror/target/debug/cstring_new_nulerror.d
+++
@@ -1,1 +0,0 @@
-/Users/globalyoung/Documents/Project/Github/rust_project/rust_release/rust1_64/013_Rust_FFI_examples/cstring_new_nulerror/target/debug/cstring_new_nulerror: /Users/globalyoung/Documents/Project/Github/rust_project/rust_release/rust1_64/013_Rust_FFI_examples/cstring_new_nulerror/src/main.rs
