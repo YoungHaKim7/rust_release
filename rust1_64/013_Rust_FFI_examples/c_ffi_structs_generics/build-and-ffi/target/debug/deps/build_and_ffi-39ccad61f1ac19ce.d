b54494c47bbd3a75194bf2c5fd7f13df9650c5d1
YoungHa Kim <ytok1108@aol.com>
2022-10-04 23:28:32 +0900

    Crust of Rust: Build Scripts and Foreign-Function Interfaces (FFI)2

--- rust1_64/013_Rust_FFI_examples/c_ffi_structs_generics/build-and-ffi/target/debug/deps/build_and_ffi-39ccad61f1ac19ce.d
+++
@@ -1,5 +0,0 @@
-/Users/globalyoung/Documents/Project/Github/rust_project/rust_release/rust1_64/013_Rust_FFI_examples/c_ffi_structs_generics/build-and-ffi/target/debug/deps/build_and_ffi-39ccad61f1ac19ce.rmeta: src/main.rs
-
-/Users/globalyoung/Documents/Project/Github/rust_project/rust_release/rust1_64/013_Rust_FFI_examples/c_ffi_structs_generics/build-and-ffi/target/debug/deps/build_and_ffi-39ccad61f1ac19ce.d: src/main.rs
-
-src/main.rs:
--- rust1_64/013_Rust_FFI_examples/c_ffi_structs_generics/build-and-ffi/target/debug/deps/build_and_ffi-9640b4c287211547.d
+++
@@ -1,5 +0,0 @@
-/Users/globalyoung/Documents/Project/Github/rust_project/rust_release/rust1_64/013_Rust_FFI_examples/c_ffi_structs_generics/build-and-ffi/target/debug/deps/build_and_ffi-9640b4c287211547.rmeta: src/main.rs
-
-/Users/globalyoung/Documents/Project/Github/rust_project/rust_release/rust1_64/013_Rust_FFI_examples/c_ffi_structs_generics/build-and-ffi/target/debug/deps/build_and_ffi-9640b4c287211547.d: src/main.rs
-
-src/main.rs:
