26ebfd6f6c305d6969c8d33212e8be5a928c7381
YoungHa Kim <ytok1108@aol.com>
2022-11-15 00:23:40 +0900

    it does compile fine

--- rust1_65/gats_generic_associated_types/src/main.rs
+++ rust1_65/gats_generic_associated_types/src/main.rs
@@ -12,10 +12,13 @@ struct WindowsMut<'t, T> {
     windows_size: usize,
 }
 
-impl<'t, T> Iterator for WindowsMut<'t, T> {
-    type Item = &'t mut [T];
+impl<'t, T> LendingIterator for WindowsMut<'t, T> {
+    type Item<'a>
+    where
+        Self: 'a,
+    = &'a mut [T];
 
-    fn next<'a>(&'a mut self) -> Option<Self::Item> {
+    fn next<'a>(&'a mut self) -> Option<Self::Item<'a>> {
         let retrval = self.slice[self.start..].get_mut(..self.windows_size)?;
         self.start += 1;
         Some(retrval)
