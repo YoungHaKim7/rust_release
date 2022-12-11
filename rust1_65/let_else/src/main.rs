72ff2321c6be00d11c0ac252fb2ee01d0a54539e
YoungHa Kim <ytok1108@aol.com>
2022-12-11 23:31:45 +0900

    let biding & irrefutable & refutable pattern

--- rust1_65/let_else/src/main.rs
+++ rust1_65/let_else/src/main.rs
@@ -1,8 +1,17 @@
-fn main() {
-    let parsed_result = "kjldkjf".parse::<i32>();
+fn main() -> Result<(), String> {
+    let parsed_result = "kjldkjf".parse::<i32>().map_err(|e| e.to_string());
+
+    // let binding
+    // pattern match
+    // irrefutable
+    let x = 9;
+    // refutable pattern
+    if let Ok(num) = parsed_result {}
+
+    // let binding
 
     let Ok(num) = parsed_result else {
-        panic!("Dear God");
+        return Err("Couldn't make a number".to_string())
     };
     println!("{num}");
 }
