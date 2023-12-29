bb33001652d0dd6f77cdad54787f7bbd2b26647a
YoungHa Kim <ytok1108@aol.com>
2023-12-30 00:03:20 +0900

    // RPIT = Return Position

--- rust1_74/d01_opaque_return_types/target/debug/deps/d01_opaque_return_types-cce7d53460105306.d
+++
@@ -1,9 +0,0 @@
-/Users/globalyoung/Documents/test/test/rust/rust_release/rust1_74/d01_opaque_return_types/target/debug/deps/libd01_opaque_return_types-cce7d53460105306.rmeta: src/main.rs Cargo.toml
-
-/Users/globalyoung/Documents/test/test/rust/rust_release/rust1_74/d01_opaque_return_types/target/debug/deps/d01_opaque_return_types-cce7d53460105306.d: src/main.rs Cargo.toml
-
-src/main.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=
-# env-dep:CLIPPY_CONF_DIR
