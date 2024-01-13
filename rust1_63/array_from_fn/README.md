677df295a0df973892cbf58d8ac2b28f1e4ac3fe
김영하 <gy-gyoung@gim-yeonghas-MacBook-Pro.local>
2024-01-13 21:57:00 +0900

    Vector to array

---
+++ rust1_63/array_from_fn/README.md
@@ -0,0 +1,8 @@
+# Result
+
+- https://stackoverflow.com/questions/26757355/how-do-i-collect-into-an-array 
+
+```
+my array : [1, 2, 3, 4, 5, 7]
+
+```
---
+++ rust1_63/array_from_fn/src/main.rs
@@ -0,0 +1,14 @@
+fn iter_to_array<Element, const N: usize>(mut iter: impl Iterator<Item = Element>) -> [Element; N] {
+    // Here I use `()` to make array zero-sized -> no real use in runtime.
+    // `map` creates new array, which we fill by values of iterator.
+    let res: [_; N] = std::array::from_fn(|_| iter.next().unwrap());
+    // Ensure that iterator finished
+    assert!(matches!(iter.next(), None));
+    res
+}
+
+fn main() {
+    let my_vec = vec![1, 2, 3, 4, 5, 7];
+    let my_array: [&i32; 6] = iter_to_array(my_vec.iter());
+    println!("my array : {:?}", my_array);
+}
