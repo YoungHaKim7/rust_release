7c8444f1ff6eab1460a460932f8c002a85ae476d
GYoung <ytok1108@aol.com>
2024-02-09 06:22:37 +0900

    println bug

--- rust1_76/a01_prinln_bug/target/debug/a01_prinln_bug.d
+++
@@ -1,1 +0,0 @@
-/Users/gy-gyoung/my_project/Rust_Lang/rust_release/rust1_76/a01_prinln_bug/target/debug/a01_prinln_bug: /Users/gy-gyoung/my_project/Rust_Lang/rust_release/rust1_76/a01_prinln_bug/src/main.rs
