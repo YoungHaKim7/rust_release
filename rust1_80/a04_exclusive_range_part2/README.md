26b63811bc4943361379c2f95f71c778b437b6ff
GYoung <ytok1108@aol.com>
2024-07-31 17:59:36 +0900

    exclusive range Rust 1.80

---
+++ rust1_80/a04_exclusive_range_part2/README.md
@@ -0,0 +1,9 @@
+# Result
+
+```bash
+
+3 is_digit : true
+1,000 is_digit : false
+1,000,000 is_digit : false
+```
+
---
+++ rust1_80/a04_exclusive_range_part2/src/main.rs
@@ -0,0 +1,14 @@
+const TOP: u32 = 10;
+const TOP_MINUS_ONE: u32 = TOP - 1;
+
+fn is_digit(x: u32) -> bool {
+    match x {
+        0..=TOP_MINUS_ONE => true,
+        TOP.. => false,
+    }
+}
+fn main() {
+    println!("3 is_digit : {}", is_digit(3));
+    println!("1,000 is_digit : {}", is_digit(1_000));
+    println!("1,000,000 is_digit : {}", is_digit(1_000_000));
+}
