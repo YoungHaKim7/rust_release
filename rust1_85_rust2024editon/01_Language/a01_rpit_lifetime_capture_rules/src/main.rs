a8c4f02695b2d81a06c52c63e7708057e1a0168f
GYoung <ytok1108@aol.com>
2024-11-30 23:37:14 +0900

    - https://doc.rust-lang.org/nightly/edition-guide/rust-2024/rpit-lifetime-capture.html

--- rust1_85_rust2024editon/01_Language/a01_rpit_lifetime_capture_rules/src/main.rs
+++ rust1_85_rust2024editon/01_Language/a01_rpit_lifetime_capture_rules/src/main.rs
@@ -1,7 +1,7 @@
 struct S<T, const C: usize>((T, [(); C]));
 impl<T, const C: usize> S<T, C> {
-//   ~~~~~~~~~~~~~~~~~
-// These generic parameters are in scope.
+    //   ~~~~~~~~~~~~~~~~~
+    // These generic parameters are in scope.
     fn f_implicit<U>() -> impl Sized {}
     //            ~       ~~~~~~~~~~
     //            ^ This generic is in scope too.
@@ -12,6 +12,30 @@ impl<T, const C: usize> S<T, C> {
     // In all editions, it's equivalent to:
     fn f_explicit<U>() -> impl Sized + use<T, U, C> {}
 }
+
+trait Tr<'a> {
+    type Ty;
+}
+impl Tr<'_> for () {
+    type Ty = ();
+}
+
+fn f_implicit() -> impl for<'a> Tr<'a, Ty = impl Copy> {}
+// In Rust 2021 and earlier, the above is equivalent to:
+fn f_2021() -> impl for<'a> Tr<'a, Ty = impl Copy + use<>> {}
+// In Rust 2024 and later, it's equivalent to:
+//fn f_2024() -> impl for<'a> Tr<'a, Ty = impl Copy + use<'a>> {}
+//                                        ~~~~~~~~~~~~~~~~~~~~
+// However, note that the capturing of higher-ranked lifetimes in
+// nested opaque types is not yet supported.
+
+// APIT
+fn f_implicit_apit(_: impl Sized) -> impl Sized {}
+//               ~~~~~~~~~~
+//           This is called APIT.
+//
+// The above is *roughly* equivalent to:
+fn f_explicit_apit<_0: Sized>(_: _0) -> impl Sized + use<_0> {}
 
 fn main() {
     println!("Rust 2024 Hello, world!");
