3d79aa4f4ea19ca30ee676f311880ef2ae7ccd5e
GYoung <ytok1108@aol.com>
2025-02-22 01:26:28 +0900

    error code

--- rust1_85_rust2024editon/9999_gen_keyword/gen_aync/target/debug/deps/gen_aync-3260bc99034e7a63.d
+++
@@ -1,5 +0,0 @@
-/home/g/my_project/Rust_Lang/9999/rust_release/rust1_85_rust2024editon/9999_gen_keyword/gen_aync/target/debug/deps/libgen_aync-3260bc99034e7a63.rmeta: src/main.rs
-
-/home/g/my_project/Rust_Lang/9999/rust_release/rust1_85_rust2024editon/9999_gen_keyword/gen_aync/target/debug/deps/gen_aync-3260bc99034e7a63.d: src/main.rs
-
-src/main.rs:
--- rust1_85_rust2024editon/9999_gen_keyword/gen_aync/target/debug/deps/gen_aync-f4734364ee724583.d
+++
@@ -1,5 +0,0 @@
-/home/g/my_project/Rust_Lang/9999/rust_release/rust1_85_rust2024editon/9999_gen_keyword/gen_aync/target/debug/deps/gen_aync-f4734364ee724583: src/main.rs
-
-/home/g/my_project/Rust_Lang/9999/rust_release/rust1_85_rust2024editon/9999_gen_keyword/gen_aync/target/debug/deps/gen_aync-f4734364ee724583.d: src/main.rs
-
-src/main.rs:
