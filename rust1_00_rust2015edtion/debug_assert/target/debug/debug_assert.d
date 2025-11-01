344660ea38c403317ccb38d8e4d88efaa3c89a4f
GYoung <ytok1108@aol.com>
2025-11-01 20:33:36 +0900

    # Rust Edition 2015(1.31.0 이전 버전은 Rust 2015 Edition)

--- rust1_00_rust2015edtion/debug_assert/target/debug/debug_assert.d
+++
@@ -1,1 +0,0 @@
-/home/gy/my_project/Rust_Lang/rust_release/rust1_00/debug_assert/target/debug/debug_assert: /home/gy/my_project/Rust_Lang/rust_release/rust1_00/debug_assert/src/main.rs
--- rust1_00_rust2015edtion/debug_assert/target/debug/deps/debug_assert-8c5030942e422697.d
+++
@@ -1,5 +0,0 @@
-/home/gy/my_project/Rust_Lang/rust_release/rust1_00/debug_assert/target/debug/deps/libdebug_assert-8c5030942e422697.rmeta: src/main.rs
-
-/home/gy/my_project/Rust_Lang/rust_release/rust1_00/debug_assert/target/debug/deps/debug_assert-8c5030942e422697.d: src/main.rs
-
-src/main.rs:
