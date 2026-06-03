2517fc9df18b874215ed364e854eb629a74422b1
GYoung <ytok1108@aol.com>
2026-06-03 19:21:53 +0900

    take _ Option

---
+++ rust1_83/d01_take/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let mut x = Some(2);
+    let y = x.take();
+    assert_eq!(x, None);
+    assert_eq!(y, Some(2));
+
+    let mut x: Option<u32> = None;
+    let y = x.take();
+    assert_eq!(x, None);
+    assert_eq!(y, None);
+}
